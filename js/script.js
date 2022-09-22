13f74909c8b0473578bf95f2e39eb9b9fc9dd78b
Bruno Pergentino <brunovpergentino@gmail.com>
2022-09-22 11:22:57 -0300

    show pokemon types

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ const RegExName = /\s/g  //RegEx for ignoring whitespaces on input
 const input = document.querySelector('.input__search');
 const type1 = document.querySelector('.type1')
 const type2 = document.querySelector('.type2')
+const icon1 = document.querySelector('.icon1')
+const icon2 = document.querySelector('.icon2')
 const buttonPrev = document.querySelector('.btn-prev');
 const buttonNext = document.querySelector('.btn-next');
 
@@ -25,9 +27,10 @@ const renderPokemon = async (pokemon) => {
   pokemonName.innerHTML = 'Loading...';
   pokemonNumber.innerHTML = '';
   pokemonImage.style.display = 'block';
-
+  icon1.className = "icon1";
+  icon2.className = "icon2";
   const data = await fetchPokemon(pokemon);
-
+  
   if (data) {
     pokemonName.innerHTML = data.name;
     pokemonNumber.innerHTML = data.id;
@@ -39,6 +42,19 @@ const renderPokemon = async (pokemon) => {
       //add static pictures while i`m searching for gifs api
       pokemonImage.src = data['sprites']['versions']['generation-vii']['ultra-sun-ultra-moon']['front_default'];
     }
+    type1Name = data["types"][0]["type"]["name"]
+    type1.src = "./images/typesImg/"+type1Name+".svg"
+    icon1.className += ` ${type1Name}`;
+    if(data["types"][1]){
+      type2.style.visibility = "visible";
+      type2Name = data["types"][1]["type"]["name"]
+      type2.src = "./images/typesImg/"+type2Name+".svg"
+      icon2.className += ` ${type2Name}`;
+    }
+    else{
+      type2.style.visibility = "hidden";
+    }
+
   } else {
     pokemonName.innerHTML = 'Not found :c';
     pokemonNumber.innerHTML = '';
